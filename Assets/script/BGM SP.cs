6e008c151f851de7ce4dd98b78e1b9a0d4d7116d
YeongCheeSeng <yeongcheeseng95@gmail.com>
2023-10-07 21:39:23 +0800

    made the code simpler

--- Assets/script/BGM SP.cs
+++ Assets/script/BGM SP.cs
@@ -1,19 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BGMSP : MonoBehaviour
 {
     public AudioSource _AudioSourceSp;
+    public Button newButton;
 
     // Start is called before the first frame update
     void Start()
     {
         _AudioSourceSp = GetComponent<AudioSource>();
-    }
+}
 
     // Update is called once per frame
     void Update()
@@ -25,5 +25,10 @@ public class BGMSP : MonoBehaviour
     {
         if (_AudioSourceSp != null)
             _AudioSourceSp.Play();
+    }
+    public void DeactivateButton()
+    {
+        Debug.Log("This button should be deactivate");
+        newButton.interactable = false;
     }
 }
